8313e44f1888365895eb4acba252ef5fa47448f5
Justin K <justinkolaras@outlook.com>
2022-02-28 00:26:05 -0800

    CS Version Core

--- class/C#/SyntaxBuilder.cs
+++ class/C#/SyntaxBuilder.cs
@@ -4,10 +4,29 @@
     The implementation and usage for this library is in Program.cs
 */
 
+using System.Collections.Generic;
+
 namespace CSVersion.SyntaxBuilder
 {
     public class ArgumentSyntaxBuilder
     {
         // To be done later..
+        private string build = "";
+        private void append(string input)
+        {
+            if (String.IsNullOrEmpty(this.build))
+            {
+                this.build = input;
+            }
+            else
+            {
+                this.build = $"{this.build} {input}";
+            }
+        }
+
+        public ArgumentSyntaxBuilder makeRegular(string input, Dictionary<string, dynamic> options)
+        {
+            return this;
+        }
     }
 }
